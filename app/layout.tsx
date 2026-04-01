ff7fa988bb29a1babd59de760d36a0d8f135fae4
estebanMns <esteban.meneses@campusucc.edu.co>
2026-04-01 01:04:30 -0500

    subiendo proycto

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Modak } from "next/font/google"; 
+import "./globals.css";
+
+const modak = Modak({
+  variable: "--font-modak",
+  subsets: ["latin"],
+  weight: "400",
+});
+
+export const metadata: Metadata = {
+  title: "Jouse te invita",
+  description: "a celebrar su primer añito",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${modak.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
